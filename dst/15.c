0fac64435719332b304254b169d8aee90f5c8421
masxman <bharathbharadwaj4321@gmail.com>
2022-12-25 20:52:50 +0530

    program 3 and 15 fixed

--- dst/15.c
+++ dst/15.c
@@ -1,122 +1,129 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<conio.h>
+
 struct node
 {
-int data;
-struct node *rlink;
-struct node *llink;
+    int data;
+    struct node *rlink;
+    struct node *llink;
 }*tmp=NULL;
 typedef struct node NODE;
+
 NODE *create();
 void preorder(NODE *);
 void inorder(NODE *);
 void postorder(NODE *);
 void insert(NODE *);
-void main()
+
+int main()
 {
-int n,i,m;
-clrscr();
-do
-{
-printf(“\n\n0.create\n\n1.insert
-\n\n2.preorder\n\n3.postorder\n\n4.inorder\n\n5.exit\n\n”);
-printf(“\n\nEnter ur choice”);
-scanf(“%d”,&m); switch(m)
-{
-case 0:
-tmp=create();
-break;
-case 1:
-insert(tmp);
-break;
-case 2:
-printf(“\n\nDisplay tree in Preorder traversal\n\n”);
-preorder(tmp);
-break;
-case 3:
-printf(“\n\nDisplay Tree in Postorder\n\n”);
-postorder(tmp);
-break;
-case 4:
-printf(“\n\nInorder\n\n”);
-inorder(tmp);
-break;
-case5:
-exit(0);
+    int m;
+    do
+    {
+        printf("\n\n0.create\n\n1.insert\n\n2.preorder\n\n3.postorder\n\n4.inorder\n\n5.exit\n\n");
+        printf("\n\nEnter your choice");
+        scanf("%d", &m); 
+        switch(m)
+        {
+            case 0:
+                tmp=create();
+                break;
+            case 1:
+                insert(tmp);
+                break;
+            case 2:
+                printf("\n\nDisplay tree in Preorder traversal\n\n");
+                preorder(tmp);
+                break;
+            case 3:
+                printf("\n\nDisplay Tree in Postorder\n\n");
+                postorder(tmp);
+                break;
+            case 4:
+                printf("\n\nInorder\n\n");
+                inorder(tmp);
+                break;
+            case 5:
+                exit(0);
+        }
+    }
+    while(m!=5);
+    return 0;
 }
-}
-while(n!=5);
-getch();
-}
+
 void insert(NODE *root)
 {
-NODE *newnode;
-if(root==NULL)
-{
-newnode=create();
-root=newnode;
-}
-else
-{
-newnode=create();
-while(1)
-{
-if(newnode->data<root->data)
-{
-if(root->llink==NULL)
-{
-root->llink=newnode;
-break;
-}
-root=root->llink;
-}
-if(newnode->data>root->data)
-{
-if(root->rlink==NULL)
-{
-root->rlink=newnode; break;
-}
-root=root->rlink;
-}
-}
-}
+    NODE *newnode;
+    if(root==NULL)
+    {
+        newnode=create();
+        root=newnode;
+    }
+    else
+    {
+        newnode=create();
+        while(1)
+        {
+            if(newnode->data<root->data)
+            {
+                if(root->llink==NULL)
+                {
+                    root->llink=newnode;
+                    break;
+                }
+                root=root->llink;
+            }
+            if(newnode->data>root->data)
+            {
+                if(root->rlink==NULL)
+                {
+                    root->rlink=newnode; 
+                    break;
+                }
+                root=root->rlink;
+            }
+        }
+    }
 }
 NODE *create()
 {
-NODE *newnode; int n;
-newnode=(NODE*)malloc(sizeof(NODE));
-printf(“\n\nEnter the Data “);
-scanf(“%d”,&n);
-newnode->data=n;
-newnode->llink=NULL;
-newnode->rlink=NULL;
-return(newnode);
+    NODE *newnode; 
+    int n;
+    newnode=(NODE*)malloc(sizeof(NODE));
+    printf("\n\nEnter the Data ");
+    scanf("%d", &n);
+    newnode->data=n;
+    newnode->llink=NULL;
+    newnode->rlink=NULL;
+    return(newnode);
 }
 void postorder(NODE *tmp)
 {
-if(tmp!=NULL)
-{
-postorder(tmp->llink);
-postorder(tmp->rlink);
-printf(“%d->”,tmp->data);
+    if(tmp!=NULL)
+    {
+        postorder(tmp->llink);
+        postorder(tmp->rlink);
+        printf("%d->", tmp->data);
+    }
 }
-}
+
 void inorder(NODE *tmp)
 {
-if(tmp!=NULL)
-{
-inorder(tmp->llink);
-printf(“%d->”,tmp->data);
-inorder(tmp->rlink);
+    if(tmp!=NULL)
+    {
+        inorder(tmp->llink);
+        printf("%d->", tmp->data);
+        inorder(tmp->rlink);
+    }
 }
-}
+
 void preorder(NODE *tmp)
 {
-if(tmp!=NULL)
-{
-printf(“%d->”,tmp->data);
-preorder(tmp->llink);
-preorder(tmp->rlink);
+    if(tmp!=NULL)
+    {
+        printf("%d->", tmp->data);
+        preorder(tmp->llink);
+        preorder(tmp->rlink);
+    }
 }
-}
+
--- dst/3.c
+++ dst/3.c
@@ -1,62 +1,60 @@
-// Selection sort in C
 #include <stdio.h>
+
 // function to swap the the position of two elements
 void swap(int *a, int *b) {
-int temp = *a;
-*a = *b;
-*b = temp;
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
+
 void selectionSort(int array[], int size) {
-for (int step = 0; step < size - 1; step++) {
-int min_idx = step;
-for (int i = step + 1; i < size; i++) {
-if (array[i] > array[min_idx])
-min_idx = i;
+  for (int step = 0; step < size - 1; step++) {
+    int min_idx = step;
+    for (int i = step + 1; i < size; i++) {
+      if (array[i] > array[min_idx])
+        min_idx = i;
+    }
+    // put min at the correct position
+    swap(&array[min_idx], &array[step]);
+    printf("\nElements after %d iteration\n", step);
+    for(int i=0; i<size; i++){
+      printf("%d\t", array[i]);
+    }
+  }
 }
-// put min at the correct position
-swap(&array[min_idx], &array[step]);
-printf("\nElements after %d iteration\n", step);
-for(int i=0; i<size; i++){
-printf("%d\t", array[i]);
-}
-}
-}
-int main() {
-int data[10];
-int size;
-printf("Enter the number of elements for array:");
-scanf("%d", &size);
-printf("\n\n Enter elements:");
-for(int i=0; i<size; i++){
-scanf("%d", &data[i]);
-}
-printf(" entered elements are :");
-for(int i=0; i<size; i++){
-printf("%d\t", data[i]);
-}
-selectionSort(data, size);
-}
-// C program using insertion sort
-#include<stdio.h>
-int main(){
-int i, j, count, temp, number[25];
-printf("How many numbers u are going to enter?: ");
-scanf("%d",&count);
-printf("Enter %d elements: ", count);
-for(i=0;i<count;i++)
-scanf("%d",&number[i]);
+
 // Implementation of insertion sort algorithm
-for(i=1;i<count;i++){
-temp=number[i];
-j=i-1;
-while((temp<number[j])&&(j>=0)){
-number[j+1]=number[j];
-j=j-1;
+void insertionSort(int array[], int size) {
+  for(i=1;i<size;i++){
+    temp=array[i];
+    j=i-1;
+    while((temp<array[j])&&(j>=0)){
+      array[j+1]=array[j];
+      j=j-1;
+    }
+    array[j+1]=temp;
+  }
+  printf("Order of Sorted elements: ");
+  for(i=0;i<size;i++)
+    printf(" %d", array[i]);
+  return 0;
 }
-number[j+1]=temp;
+
+int main() {
+  int data[10];
+  int size;
+  printf("Enter the number of elements for array:");
+  scanf("%d", &size);
+  printf("\n\n Enter elements:");
+  for(int i=0; i<size; i++){
+    scanf("%d", &data[i]);
+  }
+  printf(" entered elements are :");
+  for(int i=0; i<size; i++){
+    printf("%d\t", data[i]);
+  }
+  selectionSort(data, size);
+  insertionSort(data, size);
+  return 0;
 }
-printf("Order of Sorted elements: ");
-for(i=0;i<count;i++)
-printf(" %d", number[i]);
-return 0;
-}
+
