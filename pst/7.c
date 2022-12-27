2ef0e9097922dab54212ea4752b080bc2d499fca
masxman <bharathbharadwaj4321@gmail.com>
2022-12-27 06:34:21 +0530

    compiled 7.c removing some errors and use '-lm' to compile it

--- pst/7.c
+++ pst/7.c
@@ -1,36 +1,40 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
+#include <float.h>
+
 int main() {
     double a, b, c, discriminant, root1, root2, realPart, imagPart;
     printf("Enter coefficients a, b and c: ");
     scanf("%lf %lf %lf", &a, &b, &c);
 
+    // Check if a is equal to 0
+    if (a == 0) {
+        printf("Invalid input: a cannot be equal to 0\n");
+        return 1;
+    }
+
     discriminant = b * b - 4 * a * c;
 
     // condition for real and different roots
-    if (discriminant > 0) {
-      printf("\n roots are real and different\n");
+    if (discriminant > DBL_EPSILON) {
+        printf("\nRoots are real and different\n");
         root1 = (-b + sqrt(discriminant)) / (2 * a);
         root2 = (-b - sqrt(discriminant)) / (2 * a);
         printf("root1 = %.2lf and root2 = %.2lf", root1, root2);
     }
-
     // condition for real and equal roots
-    else if (discriminant == 0) {
-
-      printf("\n roots are real and equal\n");
+    else if (fabs(discriminant) <= DBL_EPSILON) {
+        printf("\nRoots are real and equal\n");
         root1 = root2 = -b / (2 * a);
         printf("root1 = root2 = %.2lf;", root1);
     }
-
     // if roots are not real
     else {
-      printf("\n roots are not real\n");
+        printf("\nRoots are not real\n");
         realPart = -b / (2 * a);
         imagPart = sqrt(-discriminant) / (2 * a);
-        printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realPart, imagPart, realPart,imagPart);
+        printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realPart, imagPart, realPart, imagPart);
     }
     return 0;
 }
-
